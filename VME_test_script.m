6e23013268f9f96d5d8c148c14357da8856c192f
nazaribiomed <65868543+nazaribiomed@users.noreply.github.com>
2020-05-24 23:32:33 +0430

    Add files via upload
    The VME is a robust method when we do not need to decompose whole the intrinsic mode functions of the signal. Indeed, if we aim to achieve a particular mode of the signal VME is the best method in this regard (just with knowing an approximation of the frequency band of that specific mode of interest).

---
+++ VME_test_script.m
@@ -0,0 +1,141 @@
+% test-code for VME
+% authors: Mojtaba Nazari and S.Mahmoud Sakhaei
+% mojtaba.nazari.21@gmail.com -- smsakhaei@nit.ac.ir
+
+% M. Nazari, S. M. Sakhaei, Variational Mode Extraction: A New Efficient
+% Method to Derive Respiratory Signals from ECG, IEEE Journal of Biomedical
+% and Health Informatics, Vol. 22, No. 4, pp. 1059-1067, july 2018.
+% http://dx.doi.org/10.1109/JBHI.2017.2734074
+
+%% --------------- Start
+close all
+clear
+clc
+
+%--------------- General Preparation
+alpha = 20000;
+T = 1000;
+t = (1:T)/T;
+tau = 0;
+tol = 1e-7;
+
+
+%% -------------------------Example 1
+ex1c1=1./(1.2+cos(2*pi*t));
+ex1c2=1./(1.5+sin(2*pi*t));
+ex1c3=cos(32*pi*t+.2*cos(64*pi*t));
+f_ex1=(ex1c1+ex1c2.*ex1c3);%+.2*randn(size(ex1c1));
+omega_init_ex1 =0;
+% omega_init_ex1=0.002;
+% omega_init_ex1=0.006;
+% omega_init_ex1=0.009;
+
+
+%% --------------------------------Example 2
+ex2c1=2*cos(4*pi*t);
+ex2c2=cos(30*pi*t).*(1+cos(2*pi*t))/2;
+ex2c3=cos(80*pi*t).*(1+sin(2*pi*t))/2;
+f_ex2=ex2c1+ex2c2+ex2c3;%+.2*randn(size(ex2c1));
+omega_init_ex2=.01;
+% omega_init_ex2=0.013;
+% omega_init_ex2=0.016;
+% omega_init_ex2=0.019;
+
+
+%% -------------------------------Example 3a
+ex3ac2=2*cos(10*pi*t+10*pi*t.^2);
+ex3ac3_1=cos(60*pi*t);
+ex3ac3_1(round(length(t)/2)+1:end)=0;
+ex3ac3_1=ex3ac3_1(1:500);
+ex3ac3_2=cos(100*pi*(t)-10*pi);
+ex3ac3_2(1:round(length(t)/2))=0;
+ex3ac3_2=ex3ac3_2(501:1000);
+ex3ac3=[ex3ac3_1 ex3ac3_2];
+f_ex3a=ex3ac2+ex3ac3;%+.2*randn(size(ex3ac2));
+omega_init_ex3a=0.006;
+% omega_init_ex3a=0.009;
+% omega_init_ex3a=0.012;
+% omega_init_ex3a=0.015;
+
+
+%% -----------------------------Example 3b
+ex3bc2=2*cos(10*pi*t+10*pi*t.^2);
+ex3bc3_1=cos(60*pi*t);
+ex3bc3_1(round(length(t)/2)+1:end)=0;
+ex3bc3_11=ex3bc3_1(1:500);
+ex3bc3_2=cos(100*pi*(t)-10*pi);
+ex3bc3_2(1:round(length(t)/2))=0;
+ex3bc3_22=ex3bc3_2(501:1000);
+ex3bc3=[ex3bc3_11 ex3bc3_22];
+f_ex3b=ex3bc2+ex3bc3;%+.2*randn(size(ex3bc2));
+omega_init_ex3b=0.026;
+% omega_init_ex3b=0.029;
+% omega_init_ex3b=0.032;
+% omega_init_ex3b=0.035;
+
+
+
+%% General Visualization
+
+for i=1:5
+    
+    switch i
+        
+        case 1
+            f=f_ex1;
+            omega=omega_init_ex1;
+            reference=ex1c1;
+            name='Example 1';
+            
+        case 2
+            f=f_ex2;
+            omega=omega_init_ex2;
+            reference=ex2c2;
+            name='Example 2';
+            
+        case 3
+            f=f_ex3a;
+            omega=omega_init_ex3a;
+            reference=ex3ac2;
+            name='Example 3a';
+            
+        case 4
+            f=f_ex3b;
+            omega=omega_init_ex3b;
+            reference=ex3bc3_1;
+            name='Example 3b';
+            
+            
+    end
+    
+    u=vme(f,alpha,omega,tau,tol);
+    
+    figure
+    plot(t,reference(end,:)./max(reference(end,:)),'r-.')
+    hold on
+    plot(t,u./max(u))
+    title(name)
+    xlabel('t(sec)')
+    ylim([min(u./max(u)) max(u./max(u))])
+    
+end
+
+
+%% -------------------------- Real Data Example (ECG)
+% load('055m.mat');
+% f_ECG=val(1,:);
+% omega_init_ECG =0;
+% f=f_ECG;
+% omega=omega_init_ECG;
+% 
+% u=vme(f,alpha,omega,tau,tol);
+% 
+% 
+% figure
+% t=60*((1:length(f))/length(f));
+% plot(t,val(end,:)./max(val(end,:)),'r-.')
+% hold on
+% plot(t,u./max(u))
+% title('Example ECG')
+% xlabel('t(sec)')
+
---
+++ vme.m
@@ -0,0 +1,124 @@
+function [u_d,u_hatd,omega]=vme(signal,Alpha,omega_int,tau,tol)
+%% Variational Mode Extraction
+% authors: Mojtaba Nazari and S.Mahmoud Sakhaei
+% mojtaba.nazari.21@gmail.com -- smsakhaei@nit.ac.ir 
+% Initial release 2016-8-10 (c) 2016
+
+
+
+% Input and Parameters:
+
+% signal  - the time domain input signal 
+% Alpha   - compactness of mode constraint
+% omega_int - initial guess of mode center-frequency
+% tau     - time-step of the dual ascent. set it to 0 in the presence of
+%           high level noise.
+% tol     - tolerance of convergence criterion; typically around 1e-6
+
+
+
+% Output:
+% u       - the desired mode
+% u_hatd   - spectra of the desired mode
+% omega   - estimated mode center-frequency
+
+
+
+
+% M. Nazari, S. M. Sakhaei, Variational Mode Extraction: A New Efficient
+% Method to Derive Respiratory Signals from ECG, IEEE Journal of Biomedical
+% and Health Informatics, Vol. 22, No. 4, pp. 1059-1067, july 2018.
+
+% http://dx.doi.org/10.1109/JBHI.2017.2734074
+
+%% ------------ Part 1: Start initializing
+save_T = length(signal);
+
+%--------------- Mirroring the signal to extend
+T = save_T;
+f_mir=zeros(1,T/2);
+f_mir(1:T/2) = signal(T/2:-1:1);
+f_mir(T/2+1:3*T/2) = signal;
+f_mir(3*T/2+1:2*T) = signal(T:-1:T/2+1);
+f = f_mir;
+
+%------------- Time Domain (t -->> 0 to T)
+T = length(f);
+t = (1:T)/T;
+
+
+udiff = tol+eps; %------ update step
+
+%------------- Discretization of spectral domain 
+omega_axis = t-0.5-1/T;
+
+
+%-------- FFT of signal(and Hilbert transform concept=making it one-sided)
+f_hat = fftshift((fft(f)));
+f_hat_onesided = f_hat;
+f_hat_onesided(1:T/2) = 0;
+
+%------------ Max. number of iterations
+N = 300;
+
+%----------- Initializing omega_d
+omega_d = zeros(N, 1);
+omega_d(1) = omega_int;
+
+
+%----------- dual variables vector
+lambda = zeros(N, length(omega_axis));
+
+%---------- keeping changes of mode spectrum
+u_hat_d = zeros(N, length(omega_axis));
+
+n = 1; %------------------ main loop counter
+
+
+%% -------------- Part 2: Main loop for iterative updates
+
+while ( udiff > tol &&  n < N ) 
+    
+%------------------ update ud 
+    u_hat_d(n+1,:) = (f_hat_onesided+(u_hat_d(n,:).*...
+        (Alpha.^2).*(omega_axis - omega_d(n)).^4)+lambda(n,:)/2)...
+        ./((1+(Alpha.^2).*(omega_axis - omega_d(n)).^4).*...
+        (1+2*Alpha^2.*(omega_axis - omega_d(n)).^4));
+    
+    
+%------------------ update omega_d     
+    omega_d(n+1) = (omega_axis(T/2+1:T)*(abs(u_hat_d(n+1, T/2+1:T)).^2)')/sum(abs(u_hat_d(n+1,T/2+1:T)).^2);
+    
+    
+%------------------ update lambda (dual ascent)     
+    lambda(n+1,:) = lambda(n,:) + (tau*...
+        (f_hat_onesided-(u_hat_d(n,:)))...
+        ./(1+(Alpha.^2).*(omega_axis - omega_d(n)).^4));
+    
+    n = n+1;
+    
+    udiff = eps;
+%------------------ 1st loop criterion    
+    udiff = udiff + 1/T*(u_hat_d(n,:)-u_hat_d(n-1,:))*conj((u_hat_d(n,:)-u_hat_d(n-1,:)))';
+    
+    udiff = abs(udiff);
+    
+end
+
+%% ------------------ Part 3: Signal Reconstruction
+
+N = min(N,n);
+omega = omega_d(1:N,:);
+u_hatd = zeros(T, 1);
+u_hatd((T/2+1):T,:) = squeeze(u_hat_d(N,(T/2+1):T,:));
+u_hatd((T/2+1):-1:2,:) = squeeze(conj(u_hat_d(N,(T/2+1):T,:)));
+u_hatd(1,:) = conj(u_hatd(end,:));
+
+u_d = zeros(1,length(t));
+u_d(1,:)=real(ifft(ifftshift(u_hatd(:,1))));
+
+%----------------- Remove mirror part
+u_d = u_d(:,T/4+1:3*T/4);
+
+u_hatd=fftshift(fft(u_d(1,:)))';
+end
